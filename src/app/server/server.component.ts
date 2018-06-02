b62af38ab933c43b8e0869e7a0411a0463c173fb
masila <abelmasila@gmail.com>
2018-06-02 13:40:56 +0300

    Used the server component

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  template: './server.componet.html'
+  templateUrl: './server.component.html'
 })
 export class ServerComponent {}
