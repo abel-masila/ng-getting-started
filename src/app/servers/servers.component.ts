572cd909f00ec693c9193eee0a37ea7b948fdd30
masila <abelmasila@gmail.com>
2018-06-02 15:04:29 +0300

    Added onclick event

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -14,4 +15,8 @@ export class ServersComponent implements OnInit {
   }
 
   ngOnInit() {}
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
 }
