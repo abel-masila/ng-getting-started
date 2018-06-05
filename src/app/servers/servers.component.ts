4309dc3dd55d9b99fe0000622ecad74632e750c0
masila <abelmasila@gmail.com>
2018-06-05 22:19:07 +0300

    added directives

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
+  serverName = 'Test';
+  serverCreated = false;
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -17,6 +19,11 @@ export class ServersComponent implements OnInit {
   ngOnInit() {}
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreationStatus =
+      'Server was created! Name is ' + this.serverName;
+    this.serverCreated = true;
+  }
+  onUpdateServerName(event) {
+    this.serverName = event.target.value;
   }
 }
